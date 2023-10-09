54552655fc255abadab5919fa66d117b263ff306
jkennethdomingo <130147461+jkennethdomingo@users.noreply.github.com>
2023-10-09 21:50:04 +0800

    modifying the link in the logo

--- vue-group/src/components/Menu.vue
+++ vue-group/src/components/Menu.vue
@@ -1,7 +1,7 @@
 <template lang="">
     <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
           <div class="app-brand demo">
-            <a href="index.html" class="app-brand-link">
+            <router-Link :to="{ name: 'home' }" class="app-brand-link">
               <span class="app-brand-logo demo">
                 <svg
                   width="25"
@@ -52,7 +52,7 @@
                 </svg>
               </span>
               <span class="app-brand-text demo menu-text fw-bold ms-2">SHEESH</span>
-            </a>
+            </router-Link>
 
             <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
               <i class="bx bx-chevron-left bx-sm align-middle"></i>
